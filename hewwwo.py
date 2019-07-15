256d2fdee244c78be7dab44b6875789c244386cc
Jack Links Moisture Packets <lyons.safyre@gmail.com>
2019-07-14 20:34:09 -0600

    added the thing

---
+++ hewwwo.py
@@ -0,0 +1,10 @@
+import random
+
+def hewwwo(main_str):
+    strm = main_str
+    stri = strm.replace('r', 'w')
+    strin = stri.replace('l' , 'w')
+    string = strin.replace('n' , 'ny')
+    print(string + ' ' + random.choice(["uwu", "owo", ""]))
+
+hewwwo("Hello world")
